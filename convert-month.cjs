0e957b482f9163d46513495710deedff58fbd952
Edward Andrews-Hodgson <edward@aldreth.com>
2025-06-26 23:42:02 +0100

    Diary entries to 16 Jan plus conversion script

---
+++ convert-month.cjs
@@ -0,0 +1,43 @@
+const {readFile, writeFile} = require('fs/promises');
+const {stringify} = require('yaml');
+
+const month = 'January';
+const monthNum = '01';
+
+const basePath = `src/posts/2005/2005-${monthNum}`;
+
+const writeDiaryEntries = async () => {
+  let buff = await readFile(`./${month}.md`);
+  let text = buff.toString();
+  let chunks = text.split('#####################\n');
+
+  chunks.map(async chunk => {
+    let lines = chunk.split('\n');
+
+    let [day, ...rest] = lines;
+
+    const twoFigureDay = day.length === 1 ? `0${day}` : day;
+
+    let yaml = getYaml(day, twoFigureDay);
+
+    let content = rest.join('\n');
+
+    const newContent = `---\n${stringify(yaml)}---\n\n${content}`;
+
+    const filepath = `${basePath}-${twoFigureDay}-diary.md`;
+
+    await writeFile(filepath, newContent);
+
+    console.log(`- [x] ${filepath}`);
+  });
+};
+
+const getYaml = (day, twoFigureDay) => {
+  const title = `${day} ${month}`;
+  const date = `2005-${monthNum}-${twoFigureDay}`;
+  const permalink = `2005/${monthNum}/${twoFigureDay}/diary/`;
+
+  return {title, description: '', date, permalink, author: 'Cindy', comments: [], type: 'Diary'};
+};
+
+writeDiaryEntries().catch(console.error);
