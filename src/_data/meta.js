21e1546d460a11aa7165a9a4efc558d712e03bf9
Edward Andrews-Hodgson <edward@aldreth.com>
2025-07-23 08:17:00 +0100

    Go back to inline nav
    It clashes with the title

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -41,7 +41,7 @@ export const navigation = {
   ariaTop: 'Main',
   ariaBottom: 'Complementary',
   ariaPlatforms: 'Platforms',
-  drawerNav: true
+  drawerNav: false
 };
 export const themeSwitch = {
   title: 'Theme',
